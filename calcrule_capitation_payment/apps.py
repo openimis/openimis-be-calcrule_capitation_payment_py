370350bd4bd92484a603cb7739c6fe12f69c2708
delcroip <patrick.delcroix@swisstph.ch>
2025-04-02 13:36:17 +0200

    chore: using utils to avoid code duplication

--- calcrule_capitation_payment/apps.py
+++ calcrule_capitation_payment/apps.py
@@ -1,21 +1,15 @@
 import importlib
 import inspect
 from django.apps import AppConfig
-from calculation.apps import CALCULATION_RULES
+from calculation.apps import CALCULATION_RULES, read_all_calculation_rules
 
 from core.abs_calculation_rule import AbsStrategy
+
 
 
 MODULE_NAME = "calcrule_capitation_payment"
 DEFAULT_CFG = {}
 
-
-def read_all_calculation_rules():
-    """function to read all calculation rules from that module"""
-    for name, cls in inspect.getmembers(importlib.import_module("calcrule_capitation_payment.calculation_rule"), inspect.isclass):
-        if cls.__module__.split('.')[1] == 'calculation_rule':
-            CALCULATION_RULES.append(cls)
-            cls.ready()
 
 
 class CalcruleCapitationPaymentConfig(AppConfig):
@@ -24,4 +18,5 @@ class CalcruleCapitationPaymentConfig(AppConfig):
     def ready(self):
         from core.models import ModuleConfiguration
         cfg = ModuleConfiguration.get_or_default(MODULE_NAME, DEFAULT_CFG)
-        read_all_calculation_rules()
+        read_all_calculation_rules(MODULE_NAME, CALCULATION_RULES)
+
